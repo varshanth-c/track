2167c174eac015f54f28a77323611387d0260317
varshanth-c <155641278+varshanth-c@users.noreply.github.com>
2024-08-30 07:11:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // Use middleware
-app.use(cors());
+app.use(cors({
+    origin : ["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET","DELETE"],
+    credentials:true
+            ));
 app.use(express.json());
 
 // Connect to MongoDB
@@ -17,12 +21,4 @@ const con = require('./db/connection.js');
 app.use(require('./routes/route'));
 
 // Start server
-con.then(db => {
-    if (!db) return process.exit(1);
-
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
-}).catch(error => {
-    console.error(`Connection Failed: ${error}`);
-});
+module.exports = app;
