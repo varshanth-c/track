8361075d2c698a62413125ab421280543b5b78ac
varshanth-c <155641278+varshanth-c@users.noreply.github.com>
2024-12-22 10:21:17 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const conn = mongoose.connect(process.env.ATLAS_URI, {
+const conn = mongoose.connect("mongodb+srv://varshanthgowdaml:varsh%40567@cluster1v.lmz5b2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1v", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(db => {
